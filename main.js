7b2a152f5d6e322099119bdddd8ae279f47fc0cc
F-N-C <casanova.franco@hotmail.com>
2017-04-09 13:04:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 var cookies = 0;
+var cursors = 0:
 
 function cookieClick(number){
   cookies = cookies + number;
+  document.getElementById("cookies").innerHTML = cookies;
 }
 
+function buyCursor(){
+  var cursorCost = Math.floor(10*Math.pow(1.1,cursors));
+  if (cookies >= cursorCost){
+    cursors = cursors + 1;
+    cookies = cookies - cursorCost:
+    document.getElementById("cookies").innerHTML = cookies;
+    document.getElementById("cursors").innerHTML = cursors;
+  };
+  var nextCost = Math.floor(10*Math.pow(1.1,cursors));
+  document.getElementById("cursorCost").innerHTML = nextCost;
+};
+
+window.setInterval(function(){
+  cookieClick (cursors);
+}, 1000);
